3d0d04182730c8147fc8a724bab8572ed3753a9a
Marcos Michel Machado Koch <marcos_koch@hotmail.com>
2023-07-28 13:37:24 -0300

    Initial Commit

---
+++ UberSwiftUI/UberSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberSwiftUIApp.swift
+//  UberSwiftUI
+//
+//  Created by Marcos Michel on 28/07/23.
+//
+
+import SwiftUI
+
+@main
+struct UberSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
