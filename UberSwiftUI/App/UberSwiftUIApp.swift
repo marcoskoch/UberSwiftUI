8604ade1dcecbc643ceec1345b609b2c2733373a
Marcos Michel Machado Koch <marcos_koch@hotmail.com>
2023-08-07 17:04:59 -0300

    create map view

--- UberSwiftUI/App/UberSwiftUIApp.swift
+++ UberSwiftUI/App/UberSwiftUIApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct UberSwiftUIApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
         }
     }
 }
---
+++ UberSwiftUI/Core/Home/Views/HomeView.swift
@@ -0,0 +1,21 @@
+//
+//  HomeView.swift
+//  UberSwiftUI
+//
+//  Created by Marcos Michel on 28/07/23.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        UberMapViewRepresentable()
+            .ignoresSafeArea()
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
