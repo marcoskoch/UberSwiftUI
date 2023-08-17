893fc92c7c013b02b082546288758c8c7108b992
Marcos Michel Machado Koch <marcos_koch@hotmail.com>
2023-08-17 12:20:43 -0300

    searching for a location

--- UberSwiftUI/Core/LocationSearch/Views/LocationSearchView.swift
+++ UberSwiftUI/Core/LocationSearch/Views/LocationSearchView.swift
@@ -10,7 +10,8 @@ import SwiftUI
 struct LocationSearchView: View {
     
     @State private var startLocationText = ""
-    @State private var destinationLocationText = "" 
+    @State private var destinationLocationText = ""
+    @StateObject var viewModel = LocationSearchViewModel()
     
     var body: some View {
         VStack {
@@ -36,7 +37,7 @@ struct LocationSearchView: View {
                         .background(Color(.systemGroupedBackground))
                         .padding(.leading)
                     
-                    TextField("Where to?", text: $destinationLocationText)
+                    TextField("Where to?", text: $viewModel.queryFragment)
                         .frame(height: 32)
                         .background(Color(.systemGray4))
                         .padding(.leading)
@@ -52,8 +53,8 @@ struct LocationSearchView: View {
             
             ScrollView {
                 VStack(alignment: .leading) {
-                    ForEach(0 ..< 20, id: \.self) { _ in
-                        LocationSearchResultCell()
+                    ForEach(viewModel.results, id: \.self) { result in
+                        LocationSearchResultCell(title: result.title, subtitle: result.subtitle)
                     }
                 }
             }
