c73c3a6bd2467933a1be362d79b49ce9842390b7
Marcos Michel Machado Koch <marcos_koch@hotmail.com>
2023-08-29 10:02:23 -0300

    add ride request view

---
+++ UberSwiftUI/Core/Trips/RideRequestView.swift
@@ -0,0 +1,145 @@
+//
+//  RideRequestView.swift
+//  UberSwiftUI
+//
+//  Created by Marcos Michel on 22/08/23.
+//
+
+import SwiftUI
+
+struct RideRequestView: View {
+    var body: some View {
+        VStack {
+            Capsule()
+                .fill(Color(.systemGray5))
+                .frame(width: 48, height: 6)
+            
+            // trip info view
+            HStack {
+                VStack {
+                    Circle()
+                        .fill(Color(.systemGray3))
+                        .frame(width: 8, height: 8)
+                    
+                    Rectangle()
+                        .fill(Color(.systemGray3))
+                        .frame(width: 1, height: 32)
+                    
+                    Rectangle()
+                        .fill(.black)
+                        .frame(width: 8, height: 8)
+                }
+                
+                VStack(alignment: .leading, spacing: 24) {
+                    HStack {
+                        Text("Current location")
+                            .font(.system(size: 16, weight: .semibold))
+                            .foregroundColor(.gray)
+                        
+                        Spacer()
+                        
+                        Text("1:30 PM")
+                            .font(.system(size: 14, weight: .semibold))
+                            .foregroundColor(.gray)
+                    }
+                    .padding(.bottom, 10)
+                    
+                    HStack {
+                        Text("Crossfit Mutantes")
+                            .font(.system(size: 16, weight: .semibold))
+                        
+                        Spacer()
+                        
+                        Text("1:55 PM")
+                            .font(.system(size: 14, weight: .semibold))
+                            .foregroundColor(.gray)
+                    }
+                }
+                .padding(.leading, 8)
+            }
+            .padding()
+            
+            Divider()
+            
+            // ride type selection view
+            
+            Text("SUGGESTED RIDES")
+                .font(.subheadline)
+                .fontWeight(.semibold)
+                .padding()
+                .foregroundColor(.gray)
+                .frame(maxWidth: .infinity, alignment: .leading)
+            
+            ScrollView(.horizontal) {
+                HStack(spacing: 12) {
+                    ForEach(0 ..< 3, id: \.self) { _ in
+                        VStack(alignment: .leading) {
+                            Image("uber-x")
+                                .resizable()
+                                .scaledToFit()
+                            
+                            VStack(spacing: 4) {
+                                Text("UberX")
+                                    .font(.system(size: 14, weight: .semibold))
+                                
+                                Text("$22.04")
+                                    .font(.system(size: 14, weight: .semibold))
+                            }
+                            .padding(8)
+                        }
+                        .frame(width: 112, height: 140)
+                        .background(Color(.systemGroupedBackground))
+                        .cornerRadius(10)
+                    }
+                }
+            }
+            .padding(.horizontal)
+            
+            // payment option view
+            
+            HStack(spacing: 12) {
+                Text("Visa")
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    .padding(6)
+                    .background(.blue)
+                    .cornerRadius(4)
+                    .foregroundColor(.white)
+                    .padding(.leading)
+                
+                Text("**** 1234")
+                    .fontWeight(.bold)
+                
+                Spacer()
+                
+                Image(systemName: "chevron.right")
+                    .imageScale(.medium)
+                    .padding()
+            }
+            .frame(height: 50)
+            .background(Color(.systemGroupedBackground))
+            .cornerRadius(10)
+            .padding(.horizontal)
+            
+            // request ride button
+            
+            Button {
+                
+            } label: {
+                Text("CONFIRM RIDE")
+                    .fontWeight(.bold)
+                    .frame(width: UIScreen.main.bounds.width - 32, height: 50)
+                    .background(.blue)
+                    .cornerRadius(10)
+                    .foregroundColor(.white)
+            }
+
+        }
+    }
+}
+
+struct RideRequestView_Previews: PreviewProvider {
+    static var previews: some View {
+        RideRequestView()
+    }
+}
