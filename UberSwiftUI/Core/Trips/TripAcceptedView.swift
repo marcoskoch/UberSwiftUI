ed20492847ad6e5c7b437f76881034788e496453
Marcos Michel Machado Koch <marcos_koch@hotmail.com>
2023-10-25 14:28:33 -0300

    LoadingView and TripAcceptedView UI

--- UberSwiftUI/Core/Trips/TripAcceptedView.swift
+++ UberSwiftUI/Core/Trips/TripAcceptedView.swift
@@ -10,12 +10,103 @@ import SwiftUI
 struct TripAcceptedView: View {
     var body: some View {
         VStack {
-            Text("Trip accepted")
+            Capsule()
+                .fill(Color(.systemGray5))
+                .frame(width: 48, height: 6)
+                .padding(.top, 8)
+            
+            VStack {
+                HStack {
+                    Text("Meet your driver at Apple Campus from you trip to Starbucks")
+                        .font(.body)
+                        .frame(height: 44)
+                        .lineLimit(2)
+                        .padding(.trailing)
+                    
+                    Spacer()
+                    
+                    VStack {
+                        Text("10")
+                        
+                        Text("min")
+                    }
+                    .frame(width: 56, height: 56)
+                    .foregroundColor(.white)
+                    .background(Color(.systemBlue))
+                    .cornerRadius(10)
+                    .bold()
+                }
                 .padding()
+                
+                Divider()
+            }
+            
+            VStack {
+                HStack {
+                    Image("perfil")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 80, height: 80)
+                        .clipShape(Circle())
+                    
+                    VStack(alignment: .leading, spacing: 4) {
+                        Text("Kevin Smith")
+                            .fontWeight(.bold)
+                        
+                        HStack {
+                            Image(systemName: "star.fill")
+                                .foregroundColor(Color(.systemYellow))
+                                .imageScale(.small)
+                            
+                            Text("4.8")
+                                .font(.footnote)
+                                .foregroundColor(.gray)
+                        }
+                    }
+                    
+                    Spacer()
+                    
+                    VStack(alignment: .center) {
+                        Image("uber-x")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 120, height: 64)
+                        
+                        HStack {
+                            Text("Mercedes S -")
+                                .font(.system(size: 14, weight: .semibold))
+                                .foregroundColor(.gray)
+                            
+                            Text("5G43K08")
+                                .font(.system(size: 14, weight: .semibold))
+                            
+                        }
+                        .frame(width: 165)
+                        .padding(.bottom)
+                    }
+                    
+                }
+                
+                Divider()
+            }
+            .padding()
+            
+            Button {
+                
+            } label: {
+                Text("CANCEL RIDE")
+                    .fontWeight(.bold)
+                    .frame(width: UIScreen.main.bounds.width - 32, height: 50)
+                    .background(.red)
+                    .cornerRadius(10)
+                    .foregroundColor(.white)
+            }
+            
         }
         .padding(.bottom, 24)
         .background(Color.theme.backgroundColor)
         .cornerRadius(16)
+        .shadow(color: Color.theme.secondaryBackgroundColor, radius: 20)
     }
 }
 
--- UberSwiftUI/Core/Trips/TripLoadingView.swift
+++ UberSwiftUI/Core/Trips/TripLoadingView.swift
@@ -10,12 +10,33 @@ import SwiftUI
 struct TripLoadingView: View {
     var body: some View {
         VStack {
-            Text("Finding you a ride...")
+            Capsule()
+                .fill(Color(.systemGray5))
+                .frame(width: 48, height: 6)
+                .padding(.top, 8)
+            
+            HStack {
+                VStack(alignment: .leading, spacing: 12) {
+                    Text("Finding you a ride...")
+                        .font(.headline)
+                    
+                    Text("Arriving at 1:30 PM")
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .foregroundColor(Color(.systemBlue))
+                }
                 .padding()
+                
+                Spacer()
+                
+                Spinner(lineWidth: 6, height: 64, width: 64)
+                    .padding()
+            }
         }
         .padding(.bottom, 24)
         .background(Color.theme.backgroundColor)
         .cornerRadius(16)
+        .shadow(color: Color.theme.secondaryBackgroundColor, radius: 20)
     }
 }
 
